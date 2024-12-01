ffb494c89a8c7b1b8b97f2a60fbff39aab3e312e
deniz2104 <sined2101@gmail.com>
2024-12-01 18:58:10 +0200

    add loader for pages

--- dealer_auto.php
+++ dealer_auto.php
@@ -14,9 +14,8 @@ if (isset($_SESSION['admin_name'])){
 <!DOCTYPE html>
 <html lang="en">
 <!TODO: Loader>
-<!TODO: Loader pentru o prima pagina inainte de asta unde aleg intre client si (dealer/admin)>
+<!TODO: Loader pentru o prima pagina>
 <!TODO: In loc de alert din javascript sa fac un alertbox tot js cusotmizat si sa am real time fetching cand dau refresh>
-<!TODO: Sa fac o pagina de prezentare a firmei,testimoniale,aspect general,posibilitate de login ca client sau ca dealer>
 <!TODO: Loader diferit fata de cel din navigarea dintre pagini la dealeri>
 <!TODO: Sa am formularul colorat border,ceva in aceaasi nuanta>
 <!TODO: Sa am un buton back to homepage>
@@ -25,47 +24,75 @@ if (isset($_SESSION['admin_name'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="styles.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <title>Log in Form</title>
 </head>
+<div class="loader-wrapper">
+    <span class="dot"></span>
+    <span class="dot"></span>
+    <span class="dot"></span>
+    <span class="dot"></span>
+</div>
 
+<div class="message" id="message">
+    <h2>Welcome Dealer!</h2>
+</div>
 <header>
-        <nav class="navbar">
-            <ul>
-                <li> <a href="homepage.php"><span></span>Homepage</a></li>
-            </ul>
-        </nav>
+    <nav class="hidden-homepage navbar">
+        <ul>
+            <li> <a href="homepage.php"><span></span>Homepage</a></li>
+        </ul>
+    </nav>
 </header>
+</div>
 
 <body>
-    <form action="login.php" method="post">
-    
-        <h1>Log in</h1>
-        <label for="nume">Nume:</label>
-        <input type="text" name="nume" id="nume" required placeholder="Introduceti numele" autocomplete="off">
-        <br>
+    <div class="hidden-homepage content">
+        <form action="login.php" method="post">
 
-        <label for="prenume">Prenume:</label>
-        <input type="text" name="prenume" id="prenume" required placeholder="Introduceti prenumele" autocomplete="off">
-        <br>
+            <h1>Log in</h1>
+            <label for="nume">Nume:</label>
+            <input type="text" name="nume" id="nume" required placeholder="Introduceti numele" autocomplete="off">
+            <br>
 
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email" required placeholder="example@gmail.com" autocomplete="off">
-        <br>
+            <label for="prenume">Prenume:</label>
+            <input type="text" name="prenume" id="prenume" required placeholder="Introduceti prenumele"
+                autocomplete="off">
+            <br>
+
+            <label for="email">Email:</label>
+            <input type="email" name="email" id="email" required placeholder="example@gmail.com" autocomplete="off">
+            <br>
 
 
-        <label for="id">ID:</label>
-        <input type="text" name="id" id="id" required placeholder="..." autocomplete="off">
-        <br>
+            <label for="id">ID:</label>
+            <input type="text" name="id" id="id" required placeholder="..." autocomplete="off">
+            <br>
 
-        <label>Role:</label>
-        <br>
-        <select name="role" required>
-            <option value="admin">Admin</option>
-            <option value="seller">Seller</option>
-        </select>
-        <button type="submit" class="btn">Log in</button>
-        <p>Don't have an account?<a href="register_form.php">Register now</a></p>
-    </form>
+            <label>Role:</label>
+            <br>
+            <select name="role" required>
+                <option value="admin">Admin</option>
+                <option value="seller">Seller</option>
+            </select>
+            <button type="submit" class="btn">Log in</button>
+            <p>Don't have an account?<a href="register_form.php">Register now</a></p>
+        </form>
+    </div>
+    <script>
+        $(document).ready(function () {
+            setTimeout(function () {
+                $('#message').fadeOut(2500, function () {
+                    setTimeout(function () {
+                        $('.loader-wrapper').fadeOut(1500, function () {
+                            $('.navbar').removeClass('hidden-homepage').addClass('visible');
+                            $('.content').removeClass('hidden-homepage').addClass('visible');
+                        });
+                    }, 1500);
+                });
+            }, 1);
+        });
+    </script>
 </body>
 
 </html>
--- seller_dashboard.php
+++ seller_dashboard.php
@@ -16,36 +16,60 @@ if(!$result){
 ?>
 
 <header>
-        <nav class="navbar">
-            <ul>
-                <li> <a href="table_of_cars.php">Cars list</a></li>
-                <li><a href="car_register_form.php">Register a car</a></li>
-                <li><a href="delete_car_form.php">Delete a car</a></li>
-                <li><a href="edit_car_form.php">Edit a car</a></li>
-            </ul>
-        </nav>
+    <nav class="hidden-homepage navbar">
+        <ul>
+            <li> <a href="table_of_cars.php"><span></span>Cars list</a></li>
+            <li><a href="car_register_form.php"><span></span>Register a car</a></li>
+            <li><a href="delete_car_form.php"><span></span>Delete a car</a></li>
+            <li><a href="edit_car_form.php"><span></span>Edit a car</a></li>
+        </ul>
+    </nav>
 </header>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="styles.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
     <title>Seller Page</title>
 </head>
+
 <body>
+    <style>
+        .loader:before {
+            content: "Welcome Seller";
+        }
+    </style>
 
-<div class="container">
+    <div class="loader"></div>
 
-<div class="content">
-   <h3>Hi, <span>seller</span></h3>
-   <h1>Welcome, <span><?php echo htmlspecialchars($_SESSION['seller_name']); ?></span></h1>  
-   <a href="dealer_auto.php" class="btn">Login</a>
-   <a href="upgrade_role_form.php" class="btn">Upgrade to admin role</a>
-   <a href="logout.php" class="btn">Logout</a>
-</div>
+    <div class="hidden-homepage container">
 
-</div>
+        <div class="content">
+            <h3>Hi, <span>seller</span></h3>
+            <h1>Welcome, <span>
+                    <?php echo htmlspecialchars($_SESSION['seller_name']); ?>
+                </span></h1>
+            <a href="dealer_auto.php" class="btn">Login</a>
+            <a href="upgrade_role_form.php" class="btn">Upgrade to admin role</a>
+            <a href="logout.php" class="btn">Logout</a>
+        </div>
+    </div>
+    <script>
+        $(document).ready(function () {
+            setTimeout(function () {
+                setTimeout(function () {
+                    $('.loader').fadeOut(1500, function () {
+                        $('.navbar').removeClass('hidden-homepage').addClass('visible');
+                        $('.container').removeClass('hidden-homepage').addClass('visible');
+                    });
+                }, 1500);
+            });
+        }, 1);
+    </script>
 </body>
+
 </html>
